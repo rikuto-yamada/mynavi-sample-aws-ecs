bd0301a9cdf060b8244f6a501fe4ce58cf34b18b
rikuto-yamada <106088357+rikuto-yamada@users.noreply.github.com>
2022-05-23 23:23:04 +0900

    Add files via upload

---
+++ backend-for-frontend/src/main/java/org/debugroom/mynavi/sample/ecs/backendforfrontend/app/web/ServiceProperties.java
@@ -0,0 +1,15 @@
+package org.debugroom.mynavi.sample.ecs.backendforfrontend.app.web;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+@ConfigurationProperties(prefix="service")
+public class ServiceProperties {
+
+    private String dns;
+
+}
---
+++ backend/src/main/java/org/debugroom/mynavi/sample/ecs/backend/config/App.java
@@ -0,0 +1,15 @@
+package org.debugroom.mynavi.sample.ecs.backend.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
+
